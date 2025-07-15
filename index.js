9d7ef5bf175256b7275acacc4af79a1d1164a021
Muhammad Abdullah khan <khan.abdullah135790@gmail.com>
2025-07-15 10:44:11 -0700

    index.js

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// import express from 'express'
-// import bodyParser from 'body-parser';
-// import mongoose from 'mongoose';
-// import userRoutes from './routes/users.js';
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const app = express();
-// const PORT = 5000;
-
-// app.use(bodyParser.json())
-// app.use('/users',userRoutes)
-
-
-
-// // MongoDB URI (replace with yours from Atlas)
-// const CONNECTION_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/localdb';
-
-// mongoose.connect(CONNECTION_URL)
-//   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-//   .catch((error) => console.error('Connection error:', error.message));
-
 import express from 'express'
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -44,21 +22,5 @@ mongoose.connect(CONNECTION_URL)
 
 
 
-
-
-// app.get('/',(req,res)=>{
-//     console.log('[TEST]!')
-//     res.send('Hello From HomePage')
-// });
-
-
-
-
-
-
-// // app.get('/',(req,res)=>{
-// //     console.log('[TEST]!')
-// //     res.send('Hello From HomePage')
-// // });
 
 
