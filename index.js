d03098fa6e6c7a627b4b6c07b57352a07c7fd6e5
Muhammad Abdullah khan <khan.abdullah135790@gmail.com>
2025-07-15 09:52:52 -0700

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = 5000;
@@ -12,7 +14,7 @@ app.use('/users',userRoutes)
 
 
 // MongoDB URI (replace with yours from Atlas)
-const CONNECTION_URL = 'mongodb+srv://Nodedb:Nodedb@nodeapi.rt2vhe6.mongodb.net/?retryWrites=true&w=majority&appName=NodeAPI';
+const CONNECTION_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/localdb';
 
 mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
